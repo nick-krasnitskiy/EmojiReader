217280bd3ba2817b0485246eec80193c42a23410
NickKrasnitskiy <nick.krasnitskiy@gmail.com>
2021-06-07 13:04:55 +0300

    Initial Commit

---
+++ EmojiReader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmojiReader
+//
+//  Created by Nick Krasnitskiy on 07.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
