5311f612bc0477e3f44f5a28aa4024388794587e
NickKrasnitskiy <nick.krasnitskiy@gmail.com>
2021-06-07 15:05:53 +0300

    The initial commit.

---
+++ EmojiReader/EmojiModel.swift
@@ -0,0 +1,15 @@
+//
+//  EmojiModel.swift
+//  EmojiReader
+//
+//  Created by Nick Krasnitskiy on 07.06.2021.
+//
+
+import Foundation
+
+struct Emoji {
+    var emoji: String
+    var name: String
+    var description: String
+    var isFavourite: Bool
+}
---
+++ EmojiReader/EmojiTableViewController.swift
@@ -0,0 +1,58 @@
+//
+//  EmojiTableViewController.swift
+//  EmojiReader
+//
+//  Created by Nick Krasnitskiy on 07.06.2021.
+//
+
+import UIKit
+
+class EmojiTableViewController: UITableViewController {
+    
+    var objects = [
+        Emoji(emoji: "🥰", name: "Love", description: "Let's love each others", isFavourite: false),
+        Emoji(emoji: "⚽️", name: "Football", description: "Let's play football together", isFavourite: false),
+        Emoji(emoji: "🐈", name: "Cat", description: "Cat is the cutest animal", isFavourite: false)
+    ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        
+        self.title = "Emoji Reader"
+        self.navigationItem.leftBarButtonItem = self.editButtonItem
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return objects.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "emojiCell", for: indexPath) as! EmojiTableViewCell
+        let object = objects[indexPath.row]
+        cell.set(object: object)
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    }
+
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            objects.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+}
